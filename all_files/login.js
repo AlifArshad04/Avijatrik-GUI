a085307c46f53130744ba0f772a3d0f298c334ae
Phantomnois <abir.hossain2107@gmail.com>
2021-03-25 21:10:58 +0600

    Added username and password to .js file

--- all_files/login.js
+++ all_files/login.js
@@ -1,26 +1,35 @@
-const name = document.getElementById('name')
+const username = document.getElementById('username')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 form.addEventListener('submit', (e) => {
     let messages = []
-    if(name.value === '' || name.value == null) {
+
+    if(username.value === '' || username.value == null) {
         messages.push('Name is required!')
     }
     
-    if(password.value.length <= 8) {
+    else if(password.value.length <= 8) {
         messages.push('Password must be longer than 8 characters')
     }
 
-    if(password.value.length >= 20) {
+    else if(password.value.length >= 20) {
         messages.push('Password must be smaller than 20 characters')
     }
 
-    /*if(password.value === 'password') {
+    else if(password.value === 'password') {
         messages.push('Password can not be password')
-    }*/
+    }
 
-    if(messages.length > 0) {
+    else if(username.value !== 'UserID1' ) {
+        messages.push("Incorrect Username!")
+    }
+
+    else if( password.value !== "AccessCode1") {
+        messages.push("Incorrect Password!")
+    }
+
+    else if(messages.length > 0) {
         e.preventDefault()
         errorElement.innerText = messages.join(', ')
     }
